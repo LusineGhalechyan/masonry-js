a0ce7b8775204cdf43b6a0deef9ceaae8c057d13
Lusine Ghalechyan <ghalechyan@yahoo.com>
2022-04-04 02:10:26 +0400

    desktop masonry layout

--- masonry.js
+++ masonry.js
@@ -8,134 +8,74 @@ Masonry.prototype.containerImgs = function () {
   if (this.allImgs) {
     return this.allImgs;
   }
-  container = document.querySelector(this.className);
+  var container = document.querySelector(this.className);
   if (container) {
     this.allImgs = container.querySelectorAll("img");
     return this.allImgs;
   }
 };
 
-Masonry.prototype.getMinHeightImgAndIndex = function () {
-  allImgs = this.containerImgs();
-  if (allImgs) {
-    minHeight = allImgs[0].clientHeight;
-    minHeightImage = allImgs[0];
-    minHeightImg = {};
-  }
-
-  if (allImgs) {
-    for (i = 1; i < allImgs.length; i++) {
-      if (columnQtyImgs[i].clientHeight < minHeight) {
-        minHeight = allImgs[i].clientHeight;
-        minHeightImage = allImgs[i];
-        minHeightImg[`${i}`] = [minHeightImage, minHeight];
+Masonry.prototype.getMinHeightIndexes = function (arr) {
+  var array = [...arr];
+  var indexes = [];
+  for (let i = 0; i < array.length; i++) {
+    var min = 10000;
+    var minIndex = 0;
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] < min && array[j] > 0) {
+        min = array[j];
+        minIndex = j;
       }
     }
+    indexes.push(minIndex);
+    array[minIndex] = -1;
   }
-  return minHeightImg;
-};
-
-Masonry.prototype.getMinValueIndex = function (arr) {
-  minEl = arr[0];
-  index = 0;
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < minEl) {
-      minEl = arr[i];
-      index = i;
-    }
-  }
-  return index;
+  return indexes;
 };
 
 Masonry.prototype.render = function () {
-  self = this;
-  layoutOptions = this.layoutOptions;
-  columnWidth = layoutOptions.columnWidth;
-  innerWidth = window.innerWidth;
-  allImgs = Array.from(this.containerImgs());
+  var { layoutOptions } = this;
+  var imgWidth = layoutOptions.columnWidth;
+  var innerWidth =
+    document.querySelector(this.className).clientWidth || window.innerWidth;
+  var allImgs = Array.from(this.containerImgs());
+  var columnQty = parseInt(innerWidth / imgWidth);
+  var sliceImgs = allImgs.slice(0, columnQty);
+  sliceImgs.forEach((img) => img.setAttribute("width", imgWidth));
+  var iterImgHeights = sliceImgs.map((e) => e.clientHeight);
 
-  document.addEventListener(
-    "DOMContentLoaded",
-    function () {
-      imgWidth = columnWidth;
-      allImgs.forEach((img) => img.setAttribute("width", imgWidth));
-      columnQty = parseInt(innerWidth / layoutOptions.columnWidth);
-      imgHeights = allImgs.map((img) => img.clientHeight);
-
-      iterImgHeights = [];
-      for (let j = 0; j < imgHeights.length; j += columnQty) {
-        for (let i = 0; i < columnQty; i++) {
-          iterImgHeights.push(imgHeights[i + j]);
-        }
-        for (let i = 0; i < columnQty; i++) {
-          minIndex = self.getMinValueIndex(iterImgHeights);
-          if (allImgs[i + j + columnQty]) {
-            allImgs[i + j + columnQty].style["position"] = "absolute";
-            allImgs[i + j + columnQty].style[
-              "top"
-            ] = `${iterImgHeights[minIndex]}px`;
-            allImgs[i + j + columnQty].style["left"] = `${
-              minIndex * imgWidth
-            }px`;
-            iterImgHeights[minIndex] =
-              iterImgHeights[minIndex] +
-              allImgs[i + j + columnQty].clientHeight;
-          }
-        }
-      }
-    },
-    { once: true }
-  );
-
-  Masonry.prototype.onResize = function () {
-    clearTimeout(this.resizeTimer);
-    this.resizeTimer = setTimeout(() => {
-      this.render();
-    }, 300);
-  };
-
-  if (this.layoutOptions.autoResize) {
-    window.addEventListener("resize", () => {
-      this.onResize;
-    });
-  }
-
-  Masonry.prototype.destroy = function () {
-    if (this.layoutOptions.autoResize) {
-      window.removeEventListener("resize", this.onResize);
+  for (let j = 0; j < allImgs.length; j += columnQty) {
+    // if (sliceImgs.length == 0) break;
+    var indexes = this.getMinHeightIndexes(iterImgHeights);
+    for (let i = 0; i < indexes.length; i++) {
+      var currEl = allImgs[i + j + columnQty];
+      if (!currEl) continue;
+      currEl.setAttribute("width", imgWidth);
+      currEl.style["position"] = "absolute";
+      currEl.style["left"] = `${indexes[i] * imgWidth}px`;
+      currEl.style["top"] = `${iterImgHeights[indexes[i]]}px`;
+      iterImgHeights[indexes[i]] += currEl.clientHeight;
     }
-  };
+  }
+};
 
-  return;
+if (this.layoutOptions.autoResize) {
+  window.addEventListener("resize", () => {
+    this.onResize;
+  });
+}
 
-  var handle;
+Masonry.prototype.onResize = function () {
+  alert(`hello`);
+  clearTimeout(this.resizeTimer);
+  this.resizeTimer = setTimeout(() => {
+    this.render();
+  }, 300);
+};
+
+Masonry.prototype.destroy = function () {
+  alert(`hello`);
   if (this.layoutOptions.autoResize) {
-    window.addEventListener("resize", () => {
-      clearTimeout(handle);
-      handle = setTimeout(() => {
-        this.positionGridImages();
-        columnQty = parseInt(innerWidth / layoutOptions.columnWidth);
-
-        for (let i = 0; i < allImgs.length; i++) {
-          getMinHeightImg = this.getMinHeightImgAndIndex();
-          console.log(`getMinHeightImg`, getMinHeightImg);
-
-          getMinHeightImgKey =
-            getMinHeightImg && Object.keys(getMinHeightImg)[0];
-
-          console.log(`getMinHeightImgKey`, getMinHeightImgKey);
-          getPrevImgsWidth = getMinHeightImgKey * columnWidth;
-          sumTheWidthOfMinHeightImgPrevImgs = getPrevImgsWidth.toString();
-          getMinHeight = getMinHeightImg[getMinHeightImgKey][1].toString();
-          getRightestEl = allImgs[columnQty - 1];
-
-          getRightestEl.style["position"] = "relative";
-          getRightestEl.style[
-            "left"
-          ] = `${sumTheWidthOfMinHeightImgPrevImgs}px`;
-          getRightestEl.style["top"] = `${getMinHeight}px`;
-        }
-      }, 300);
-    });
+    window.removeEventListener("resize", this.onResize);
   }
 };
